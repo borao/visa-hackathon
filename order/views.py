98a4746bb332e1402ddf5e275006c5964b7eb446
CurtisWang03251 <wangc6@rose-hulman.edu>
2020-06-28 00:19:58 -0400

    add endpoints for searching gift by sender and receiver

--- order/views.py
+++ order/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 import requests
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from rest_framework import viewsets
 from rest_framework.decorators import action
 
@@ -15,15 +15,15 @@ class OrderViewSet(viewsets.ModelViewSet):
     serializer_class = OrderSerializer
 
     @action(detail = True, methods=['get'])
-    def getQRCode(selfs, request, pk):
+    def getQRCode(self, request, pk):
         requestUrl = QRCODE_API_ENDPOINT + pk
         qrCode = requests.get(url = requestUrl)
         return HttpResponse(qrCode.content, content_type="image/png")
 
     @action(detail=False, methods=['post'])
-    def purchaseGift(selfs, request):
+    def purchaseGift(self, request):
         requestData = request.data
-        serializer = selfs.get_serializer(data = requestData)
+        serializer = self.get_serializer(data = requestData)
         serializer.is_valid(raise_exception=True)
 
         response = chargeCreditCard(serializer.validated_data['giftAmount'])
@@ -31,9 +31,18 @@ class OrderViewSet(viewsets.ModelViewSet):
         serializer.save()
         return HttpResponse(responseResultCode)
 
+    # call this by adding id into the url like http://localhost:8000/orders/giftSentTo/2
+    @action(detail=False, url_path='giftSentTo/(?P<receiverID>[^/.]+)')
+    def giftSentTo(self, request, receiverID):
+        searchResult = self.queryset.filter(receiverID = receiverID).values()
+        return HttpResponse(searchResult, content_type="application/json")
+
+    @action(detail = False, url_path='giftSentBy/(?P<senderID>[^/.]+)')
+    def giftSentBy(self, request, senderID):
+        searchResult = self.queryset.filter(senderID = senderID).values()
+        return HttpResponse(searchResult, content_type="application/json")
 
     # to redeem the gift, use patch method on detail view
     # {
     #     "redeemed": true
     # }
-
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
 
 from .serializers import *
 

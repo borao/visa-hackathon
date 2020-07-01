b7e8c702d8e52e996d56e211c07ba54aa6fa73f9
Bora Oztekin <borao@users.noreply.github.com>
2020-06-30 23:39:11 -0500

    Add script for populating user data

---
+++ users/management/commands/populateData.py
@@ -0,0 +1,17 @@
+import json
+from django.core.management.base import BaseCommand
+from users.models import User
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('userData.json', type=str)
+
+    def handle(self, *args, **options):
+        with open(options['userData.json']) as f:
+            data_list = json.load(f)
+
+        for data in data_list:
+            data['pk'] = data.pop('user')
+            User.objects.get_or_create(pk=data['pk'], defaults=data)
